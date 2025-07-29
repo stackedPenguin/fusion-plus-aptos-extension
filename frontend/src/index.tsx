1e7a175fe442430b013ff9814a8abf1adb62dbf1
Wei Yang <weiyangmiami@gmail.com>
2025-07-29 10:53:40 -0500

    Fix Aptos wallet adapter package imports

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
-import { PetraWallet } from '@aptos-labs/wallet-adapter-petra';
+import { PetraWallet } from 'petra-plugin-wallet-adapter';
 
 const wallets = [new PetraWallet()];
 
